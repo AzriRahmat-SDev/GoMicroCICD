96bd1b1c8ced2c2ce267d136652b3a70c221379a
Muhammad Azri Bin Rahmat <63068226+AzriRahmat-SDev@users.noreply.github.com>
2022-06-10 09:51:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,5 +6,6 @@ func main() {
 	fmt.Println("Hello world!")
 	fmt.Println("changing the text to this")
 	fmt.Println("this is to make a new remote branch")
+	fmt.Println("this is for pull request")
 
 }
